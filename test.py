8ce0846c60ab2461e289b34e2b856f35fe81d971
lanpokn <2606629917@qq.com>
2021-11-17 23:14:01 +0800

    map_ob

--- test.py
+++ test.py
@@ -2,12 +2,34 @@ import rospy
 import tf
 from nav_msgs.srv import GetMap
 import numpy as np
-
+#resolution: 0.155
 if __name__ == "__main__":
     # getMap = rospy.ServiceProxy('/static_map',GetMap)
     # msg = getMap().map
     # data = np.array(msg.data)
     # data.resize(129,129)
-    # np.savetxt(fname='/home/lanpokn/Documents/2021/robot2/course2021-Robotics2-master/map.txt',X = data)
+    # np.save('/home/lanpokn/Documents/2021/robot2/course2021-Robotics2-master/map',data)
     # print(data[:,4])
-    data = np.
+    data1 = np.load("/home/lanpokn/Documents/2021/robot2/course2021-Robotics2-master/map.npy")
+    data2 = np.zeros(shape = (129,129))
+    avoid_dist = 2
+    for i in range(0,129):
+        for j in range(0,129):
+                temp = data1[i][j]
+                flag = 0#no obscal
+                for k in range(i-avoid_dist,i+avoid_dist):
+                    for l in range(j-avoid_dist,j+avoid_dist):
+                        if(k<0 or k>=129 or l<0 or l>=129):
+                            continue
+                        else:
+                            if(data1[k][l]== 100):
+                                flag  =1#have obscal
+                
+                if(flag == 0):
+                    data2[i][j] = -1
+                else:
+                    data2[i][j] = 100
+    np.save('/home/lanpokn/Documents/2021/robot2/course2021-Robotics2-master/map_ob',data2) 
+    print(data2[:,65])
+    
+    
